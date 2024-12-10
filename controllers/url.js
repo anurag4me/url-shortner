9a22da2384b790c675238a617a086ce7fd29c538
anurag4me <anuragmishra5033@gmail.com>
2024-12-10 13:25:16 +0530

    create analytics and url routes

--- controllers/url.js
+++ controllers/url.js
@@ -1,18 +1,29 @@
-const URL = require("../models/url")
-const shortid = require("shortid")
+const URL = require("../models/url");
+const shortid = require("shortid");
 
-async function handleGenerateNewShortUrl(req, res){
-    const body = req.body;
-    if(!body.url) return res.status(400).json({error: "url is required"})
-    const short_id = shortid()
-    await URL.create({
-        shortId: short_id,
-        redirectURL: body.url,
-        visitHistory: [],
-    })
-    return res.json({id: short_id})
+async function handleGenerateNewShortUrl(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
+  const short_id = shortid();
+  await URL.create({
+    shortId: short_id,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+  return res.json({ id: short_id });
+}
+
+async function handleGetAnalytics(req, res) {
+  const shortID = req.params.shortID;
+  const result = await URL.findOne({ shortId: shortID });
+  if (!result) return res.status(404).json({ error: "url not found" });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
 }
 
 module.exports = {
-    handleGenerateNewShortUrl
-}
+  handleGenerateNewShortUrl,
+  handleGetAnalytics,
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
 const connectMongoDb = require("./connection");
+const URL = require("./models/url");
 
 const app = express();
 const PORT = 8001;
@@ -15,5 +16,13 @@ app.use(express.json());
 
 // route
 app.use("/url", urlRoute);
+app.get("/:shortID", async (req, res) => {
+  const shortID = req.params.shortID;
+  const entry = await URL.findOneAndUpdate(
+    { shortId: shortID },
+    { $push: { visitHistory: { timestamps: Date.now() } } }
+  );
+  res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
