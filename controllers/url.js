1589cf6349566206661f0e789a49e27e2d6dae7d
anurag4me <anuragmishra5033@gmail.com>
2024-12-10 10:58:04 +0530

    replaced conflicting dependancy

--- controllers/url.js
+++ controllers/url.js
@@ -1,10 +1,10 @@
 const URL = require("../models/url")
-const {nanoid} = require("nanoid")
+const shortid = require("shortid")
 
 async function handleGenerateNewShortUrl(req, res){
     const body = req.body;
     if(!body.url) return res.status(400).json({error: "url is required"})
-    const short_id = nanoid(8)
+    const short_id = shortid()
     await URL.create({
         shortId: short_id,
         redirectURL: body.url,
